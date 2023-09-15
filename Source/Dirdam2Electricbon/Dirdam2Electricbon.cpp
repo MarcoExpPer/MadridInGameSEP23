e0eff931851c4298a709567326bb6518e2151e3d
Marco Exp <80280295+MarcoExpPer@users.noreply.github.com>
2023-09-15 20:25:27 +0200

    initial commit

---
+++ Source/Dirdam2Electricbon/Dirdam2Electricbon.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Dirdam2Electricbon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dirdam2Electricbon, "Dirdam2Electricbon" );
